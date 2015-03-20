57b9bde303bf178d69c409e9f6da3ee44efa8f7e
Whatsmename <whatsmename@Johns-Mac.local>
2015-03-20 04:33:16 -0700

    Initial Commit

---
+++ HAL-9000/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HAL-9000
+//
+//  Created by John Doe on 20/03/2015.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
